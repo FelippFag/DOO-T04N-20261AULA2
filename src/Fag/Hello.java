97360f1eafa0920b69f0b616d77bc0283808e13e
FelippFag <farpiran@minha.fag.edu.br>
2026-03-08 11:06:20 -0300

    Feature: Adicionado a classe Hello

---
+++ src/Fag/Hello.java
@@ -0,0 +1,13 @@
+package Fag;
+
+public class Hello {
+
+	public static void writeHello() {
+		System.out.println("Hello World");
+	}
+
+	public static void main(String[] args) {
+		writeHello();
+	}
+
+}
