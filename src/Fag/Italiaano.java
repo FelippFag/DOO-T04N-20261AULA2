13f2216fadaa0b6f1e926265ed5f2f654339ce0b
FelippFag <farpiran@minha.fag.edu.br>
2026-03-08 13:10:19 -0300

    Feature: Adicionado Italiaano

---
+++ src/Fag/Italiaano.java
@@ -0,0 +1,13 @@
+package Fag;
+
+public class Italiaano {
+
+	public static void writeItaliaano() {
+		System.out.println("Ciao Mondo!");
+	}
+
+	public static void main(String[] args) {
+		writeItaliaano();
+	}
+
+}
