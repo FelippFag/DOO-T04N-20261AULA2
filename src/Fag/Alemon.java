3776a285cd3eaea43abfd2730e922d8c20b261e9
FelippFag <farpiran@minha.fag.edu.br>
2026-03-08 13:08:21 -0300

    Feature: Adicionado Alemon

---
+++ src/Fag/Alemon.java
@@ -0,0 +1,13 @@
+package Fag;
+
+public class Alemon {
+
+	public static void writeAlemon() {
+		System.out.println("Hallo Welt!");
+	}
+
+	public static void main(String[] args) {
+		writeAlemon();
+	}
+
+}
